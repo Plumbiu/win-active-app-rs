47608a2e151733848906515ac099f8d6e70b9319
Plumbiu <3434909403@qq.com>
2024-01-14 14:42:54 +0800

    0.0.3

---
+++ __test__/index.js
@@ -0,0 +1,3 @@
+const { getCurrentAppPath } = require('../index.js')
+
+console.log(getCurrentAppPath())
--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,39 @@
 #![allow(unused)]
 use napi_derive::napi;
 use std::collections::HashMap;
-use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowModuleFileNameW};
+use sysinfo::{Components, Disks, Networks, System};
+use windows::Win32::UI::WindowsAndMessaging::{
+  GetForegroundWindow, GetWindowModuleFileNameW, GetWindowThreadProcessId,
+};
 use winreg::enums::HKEY_CLASSES_ROOT;
 use winreg::RegKey;
 
 const APP_NAME_SUFFIX: &str = ".FriendlyAppName";
 const EXE_SUFFIX: &str = ".exe.FriendlyAppName";
 
+fn get_all_process() -> HashMap<u32, String> {
+  let mut sys = System::new_all();
+  sys.refresh_all();
+  let mut map: HashMap<u32, String> = HashMap::new();
+  for (pid, p) in sys.processes() {
+    if let Some(exe) = p.exe() {
+      map.insert(
+        pid.as_u32(),
+        exe.to_string_lossy().to_string().to_lowercase(),
+      );
+    }
+  }
+  map
+}
+
 #[napi]
-fn get_current_app_path() -> String {
+fn get_current_app_path() -> Option<String> {
   unsafe {
     let hwd = GetForegroundWindow();
-    let mut text: [u16; 512] = [0; 512];
-    let file_path_len = GetWindowModuleFileNameW(hwd, &mut text);
-    String::from_utf16_lossy(&text[..file_path_len as usize])
+    let mut pid = 0;
+    GetWindowThreadProcessId(hwd, Some(&mut pid));
+    let all_process = get_all_process();
+    all_process.get(&pid).cloned()
   }
 }
 
@@ -27,7 +46,10 @@ fn get_cached_apps() -> HashMap<String, String> {
   let mut apps: HashMap<String, String> = HashMap::new();
   for (name, value) in system.enum_values().map(|x| x.unwrap()) {
     if name.ends_with(EXE_SUFFIX) {
-      apps.insert(name.replace(APP_NAME_SUFFIX, ""), value.to_string());
+      apps.insert(
+        name.replace(APP_NAME_SUFFIX, "").to_lowercase(),
+        value.to_string(),
+      );
     }
   }
   apps
