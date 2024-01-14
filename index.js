5a7db0e8e60bbb9b01a304c85f59be3963db9870
Plumbiu <3434909403@qq.com>
2024-01-14 15:31:04 +0800

    0.0.5

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ function isMusl() {
   // For Node 10
   if (!process.report || typeof process.report.getReport !== 'function') {
     try {
-      const lddPath = require('child_process').execSync('which ldd').toString().trim()
+      const lddPath = require('child_process')
+        .execSync('which ldd')
+        .toString()
+        .trim()
       return readFileSync(lddPath, 'utf8').includes('musl')
     } catch (e) {
       return true
@@ -29,41 +32,11 @@ function isMusl() {
 }
 
 switch (platform) {
-  case 'android':
-    switch (arch) {
-      case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'win-active-app-rs.android-arm64.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./win-active-app-rs.android-arm64.node')
-          } else {
-            nativeBinding = require('win-active-app-rs-android-arm64')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'win-active-app-rs.android-arm-eabi.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./win-active-app-rs.android-arm-eabi.node')
-          } else {
-            nativeBinding = require('win-active-app-rs-android-arm-eabi')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      default:
-        throw new Error(`Unsupported architecture on Android ${arch}`)
-    }
-    break
   case 'win32':
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'win-active-app-rs.win32-x64-msvc.node')
+          join(__dirname, 'win-active-app-rs.win32-x64-msvc.node'),
         )
         try {
           if (localFileExisted) {
@@ -77,7 +50,7 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'win-active-app-rs.win32-ia32-msvc.node')
+          join(__dirname, 'win-active-app-rs.win32-ia32-msvc.node'),
         )
         try {
           if (localFileExisted) {
@@ -91,7 +64,7 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'win-active-app-rs.win32-arm64-msvc.node')
+          join(__dirname, 'win-active-app-rs.win32-arm64-msvc.node'),
         )
         try {
           if (localFileExisted) {
@@ -105,169 +78,6 @@ switch (platform) {
         break
       default:
         throw new Error(`Unsupported architecture on Windows: ${arch}`)
-    }
-    break
-  case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'win-active-app-rs.darwin-universal.node'))
-    try {
-      if (localFileExisted) {
-        nativeBinding = require('./win-active-app-rs.darwin-universal.node')
-      } else {
-        nativeBinding = require('win-active-app-rs-darwin-universal')
-      }
-      break
-    } catch {}
-    switch (arch) {
-      case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'win-active-app-rs.darwin-x64.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./win-active-app-rs.darwin-x64.node')
-          } else {
-            nativeBinding = require('win-active-app-rs-darwin-x64')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'arm64':
-        localFileExisted = existsSync(
-          join(__dirname, 'win-active-app-rs.darwin-arm64.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./win-active-app-rs.darwin-arm64.node')
-          } else {
-            nativeBinding = require('win-active-app-rs-darwin-arm64')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      default:
-        throw new Error(`Unsupported architecture on macOS: ${arch}`)
-    }
-    break
-  case 'freebsd':
-    if (arch !== 'x64') {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
-    }
-    localFileExisted = existsSync(join(__dirname, 'win-active-app-rs.freebsd-x64.node'))
-    try {
-      if (localFileExisted) {
-        nativeBinding = require('./win-active-app-rs.freebsd-x64.node')
-      } else {
-        nativeBinding = require('win-active-app-rs-freebsd-x64')
-      }
-    } catch (e) {
-      loadError = e
-    }
-    break
-  case 'linux':
-    switch (arch) {
-      case 'x64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'win-active-app-rs.linux-x64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./win-active-app-rs.linux-x64-musl.node')
-            } else {
-              nativeBinding = require('win-active-app-rs-linux-x64-musl')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'win-active-app-rs.linux-x64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./win-active-app-rs.linux-x64-gnu.node')
-            } else {
-              nativeBinding = require('win-active-app-rs-linux-x64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      case 'arm64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'win-active-app-rs.linux-arm64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./win-active-app-rs.linux-arm64-musl.node')
-            } else {
-              nativeBinding = require('win-active-app-rs-linux-arm64-musl')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'win-active-app-rs.linux-arm64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./win-active-app-rs.linux-arm64-gnu.node')
-            } else {
-              nativeBinding = require('win-active-app-rs-linux-arm64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      case 'arm':
-        localFileExisted = existsSync(
-          join(__dirname, 'win-active-app-rs.linux-arm-gnueabihf.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./win-active-app-rs.linux-arm-gnueabihf.node')
-          } else {
-            nativeBinding = require('win-active-app-rs-linux-arm-gnueabihf')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'riscv64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'win-active-app-rs.linux-riscv64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./win-active-app-rs.linux-riscv64-musl.node')
-            } else {
-              nativeBinding = require('win-active-app-rs-linux-riscv64-musl')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'win-active-app-rs.linux-riscv64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./win-active-app-rs.linux-riscv64-gnu.node')
-            } else {
-              nativeBinding = require('win-active-app-rs-linux-riscv64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      default:
-        throw new Error(`Unsupported architecture on Linux: ${arch}`)
     }
     break
   default:
